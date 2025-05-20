66c6c9ce88aad553a02e8ff45911722ebe240bdd
chami-gunawardane <chamigunawardane@gmail.com>
2025-05-20 18:24:59 +0530

    Project initialized

---
+++ src/pages/Landing/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold  text-cyan-900">
+    Welcome to SKILLNET webpage
+  </h1>
+    </div>
+  )
+}
+
+export default index
