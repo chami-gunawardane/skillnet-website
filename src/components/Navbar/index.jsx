3263ad59ff19e88413d7f9feaafb4d712ecca569
chami-gunawardane <chamigunawardane@gmail.com>
2025-05-24 11:02:42 +0530

    Fixed navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,14 +4,16 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between px-16 py-4 bg-white font-sans p-2 border-b-2 border-gray-200 ">
+    <nav className="flex items-center justify-between px-4 py-4 bg-white font-sans p-2 border-b-2 border-gray-200 ">
       {/* Logo */}
+      <Link to="/">
       <div className="flex items-center">
         <img src={logo} alt="Skillnet Logo" className="h-10 w-auto pl-[100px]" />
       </div>
+      </Link>
 
       {/* Navigation Links */}
-      <ul className="flex space-x-20 font-medium text-xl">
+      <ul className="flex space-x-20 font-medium text-lg">
         <li className="text-black hover:text-blue-400 cursor-pointer"><Link to="/">Home</Link></li>
         <li className="text-black hover:text-blue-400 cursor-pointer"><Link to="/Domain">Domain</Link></li>
         <li className="text-black hover:text-blue-400 cursor-pointer"><Link to="/Milestones">Milestones</Link></li>
